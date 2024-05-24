1937e35051363d21f40b7086ee68271dfe193e52
Nicolas Steger <njsteger@hawaii.edu>
2024-05-23 18:14:38 -1000

    island loaded

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import { Canvas } from '@react-three-fiber'
-
-const Home = () => {
-  return (
-    <section className="w-full h-screen relative">
-      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+import Island from '../models/Island'
+/**
+ <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
       </div>
+ */
+const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
 
-      <Canvas>
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return { screenScale, screenPosition, rotation };
+  }
 
+  const { screenScale, screenPosition, rotation } = adjustIslandForScreenSize();
+
+  return (
+    <section className="w-full h-screen relative">
+
+      <Canvas className="w-full h-full bg-transparent" camera={{ near: 0.1, far: 1000}}>
+        <Suspense fallback={<Loader />}> 
+          <directionalLight  />
+          <ambientLight  />
+          <pointLight  />
+          <hemisphereLight  />
+          <Island 
+            scale={screenScale}
+            position={screenPosition}
+            rotation={rotation}
+          />
+        </Suspense>
       </Canvas>
+
     </section>
   )
 }

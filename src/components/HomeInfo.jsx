e08ac0e3727ec84533ee8772cf5893c1085f783d
Nicolas Steger <njsteger@hawaii.edu>
2024-06-05 18:19:53 -1000

    changes

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { arrow } from '../assets/icons'
 
-const HomeInfo = () => {
-  return (
-    <div>HomeInfo</div>
-  )
+const InfoBox = ({ text, link, btnText }) => (
+  <div className='info-box text-center'>
+    <p className='font-medium sm:text-xl text-center'>{text}</p>
+    <Link to={link} className='neo-brutalism-white neo-btn'>
+      {btnText}
+      <img src={arrow} className='w-4 h-4 object-contain'/>
+    </Link>
+  </div>
+)
+
+const renderContent = {
+  1: (
+    <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+      Hello, my name is <span className='font-semibold'> Nicolas Steger</span>
+      <br/>
+      A University of Hawaii Computer Sceince graduate.
+      </h1>
+  ),
+  2: (
+    <InfoBox
+     text= "I am a recent graduate from the University of Hawaii at Manoa with a Bachelor's degree in Computer Science."
+      link= "/about"
+      btnText= "Learn More about me"
+     />
+  ),
+  3: (
+    <InfoBox
+     text= "Worked on multiple projects using various technologies such as React, Node.js, and Python. I am always looking for new opportunities to learn and grow as a developer"
+      link= "/projects"
+      btnText= "Visit portfolio"
+     />
+  ),
+  4: (
+    <InfoBox
+     text= "Interested in contacting me about hiring or future projects? Feel free to reach out to me through my contact page"
+      link= "/contact"
+      btnText= "Contact me"
+     />
+  ),
 }
+
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
+  }
 
 export default HomeInfo
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,108 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+  const formRef = useRef(null);
+  const [form, setForm] = useState({ name: '', email: '', message: ''});
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    console.log('Submission started, setting loading true');
+  
+    console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: 'Nicolas Steger',
+        from_email: form.email,
+        to_email: 'Nicksteger@live.com',
+        message: form.message,
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then((result) => {
+      console.log('Email sent:', result.text);
+      setIsLoading(false);
+      setForm({ name: '', email: '', message: '' });
+      console.log('Setting loading false after email sent');
+    }).catch((err) => {
+      console.error('Error sending email:', err);
+      setIsLoading(false);
+      console.log('Setting loading false after error');
+    });
+  };
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'
+      onSubmit={handleSubmit}
+    >
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>
+          Contact
+        </h1>
+        <form className='w-full flex flex-col gap-7 mt-14'>
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input 
+            type='text' 
+            name="name" 
+            className='input' 
+            placeholder='Enter Name' 
+            required 
+            value={form.name}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input 
+            type='email' 
+            name="email" 
+            className='input' 
+            placeholder='john@gmail.com' 
+            required 
+            value={form.email}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Message
+            <textarea 
+            name="message" 
+            rows={4}
+            className='input' 
+            placeholder='Send Message' 
+            required 
+            value={form.message}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            />
+          </label>
+          <button 
+            type='submit' 
+            className='btn'
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur} 
+            >
+              {isLoading ? 'Loading...' : 'Send Message'}
+            </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
